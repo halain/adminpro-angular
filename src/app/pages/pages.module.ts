2346df326c292e77d832831706478f2da5c86e56
Hector Alvarez <halain80@gmail.com>
2019-02-21 13:23:11 -0500

    Modificaciones en los  modulos

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// Rutas
+// Rutas hijas importadas de pages.routes
 import { FeatureRoutingModule } from './pages.routes';
 
+// modulo que contiene los componentes compartidos, header, sidebar, etc
 import { SharedModule } from '../shared/shared.module';
 
-
+// compromente principal, layout del backend
 import { PagesComponent } from './pages.component';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
@@ -19,7 +21,7 @@ import { Graficas1Component } from './graficas1/graficas1.component';
     ProgressComponent,
     Graficas1Component
   ],
-  exports: [
+  exports: [ // para que estas paginas puedan ser utilizadas por componentes de otros modulos hay que esportarlas
     PagesComponent,
     DashboardComponent,
     ProgressComponent,
