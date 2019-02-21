0a6535bb96ae5a9225c0a2ce8327372903e86f1c
Hector Alvarez <halain80@gmail.com>
2019-02-21 12:06:51 -0500

    correciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Modulos
+// import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
@@ -13,6 +18,7 @@ import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
@@ -30,6 +36,7 @@ import { RegisterComponent } from './login/register.component';
   ],
   imports: [
     BrowserModule,
+    // PagesModule,
     AppRoutingModule
   ],
   providers: [],
